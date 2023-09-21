eb48abc39c8815f78f8149ebbb3a0529f56fd60d
Flips69 <aion.lipe@gmail.com>
2023-09-21 15:20:35 -0300

    Commit Listagem BuscaCep

--- src/components/BuscaCepForm.tsx
+++ src/components/BuscaCepForm.tsx
@@ -1,6 +1,9 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react'
 
 import styles from "../App.module.css"
+import Header from './Header';
+import Footer from './Footer';
+
 
 const BuscaCep = () => {
 
@@ -37,6 +40,8 @@ const BuscaCep = () => {
 
     return (
         <div>
+            <Header/>
+            <main className={styles.main}>
             <form onSubmit={findCep}>
                 <label htmlFor="cep">CEP</label>
                 <input type="text" name="cep" id="cep" 
@@ -48,6 +53,8 @@ const BuscaCep = () => {
             <p>Estado: {uf}</p>
             <p>CEP: {cep}</p> 
             <p className={styles.error}>{erro}</p>
+        <Footer/>
+        </main>
         </div>
     );
 }
